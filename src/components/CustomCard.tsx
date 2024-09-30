2fa995ad9162747d4f561892f718e43fa2bd0cf9
Pawel <finestedm@gmail.com>
2024-09-30 20:30:13 +0200

    first commit

---
+++ src/components/CustomCard.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader } from "@mui/material";
+
+export default function CustomCard() {
+    return (
+        <Card>
+            <CardHeader>
+                Test
+            </CardHeader>
+        </Card>
+    )
+}
