02b0140a8b51b5e701e5354baa9d46f9f5fa78c3
Pawel <finestedm@gmail.com>
2024-10-07 11:39:19 +0200

    shops buttons redesign

--- src/components/CustomCard/subcomponents/CardBottom.tsx
+++ src/components/CustomCard/subcomponents/CardBottom.tsx
@@ -1,5 +1,7 @@
-import { Avatar, Box, Button, ButtonGroup, IconButton, Stack } from "@mui/material";
+import { Avatar, Box, Button, ButtonGroup, Divider, IconButton, Stack } from "@mui/material";
 import ShareIcon from '@mui/icons-material/Share';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import amazonLogo from '../../../media/Amazon_(company)-Logo.wine.svg'
 
 export default function CardBottom() {
     return (
@@ -7,12 +9,14 @@ export default function CardBottom() {
             <Stack direction='row' spacing={2} justifyContent='space-between'>
                 <Box>
                     <Stack direction='row' spacing={1}>
-                        <Button disableElevation variant="contained" color="inherit">Amazon</Button>
+                        <Button variant='outlined' sx={{ height: 35, p: 0.25 }} color="primary"><img height='100%' width='100%' src={amazonLogo} /></Button>
                         <Button disableElevation variant="contained" color="inherit">Allegro</Button>
                         <Button disableElevation variant="contained" color="inherit">Ceneo</Button>
+                        <Divider orientation="vertical" />
                     </Stack>
                 </Box>
                 <Box>
+                    <IconButton> <BookmarkBorderIcon /></ IconButton>
                     <IconButton><ShareIcon /></IconButton>
                 </Box>
             </Stack>
--- src/components/CustomCard/subcomponents/OfferButtonBox.tsx
+++ src/components/CustomCard/subcomponents/OfferButtonBox.tsx
@@ -1,6 +1,5 @@
 import { Box, Button, Grid2, IconButton, Stack, Typography, useTheme } from "@mui/material";
 import GoToOfferButton from "./GoToCommentsButton";
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 
 export default function BottomButtonBox() {
@@ -10,7 +9,6 @@ export default function BottomButtonBox() {
     return (
         <Box>
             <Stack direction='row' spacing={2} justifyContent='space-between'>
-                <IconButton size='small' sx={{ borderWidth: 1, borderColor: theme.palette.divider, borderStyle: 'solid', borderRadius: 100 }}> <BookmarkBorderIcon /></ IconButton>
                 <IconButton size='small' sx={{ borderWidth: 1, borderColor: theme.palette.divider, borderStyle: 'solid', borderRadius: 100, px: 1 }}> <Stack spacing={1} direction='row'><ChatOutlinedIcon /> <Typography> 12</Typography></Stack> </ IconButton>
                 <GoToOfferButton />
             </Stack>
